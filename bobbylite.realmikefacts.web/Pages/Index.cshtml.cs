244b5275484605b2f64831d173fadee13f66a503
Bobby <bobbielite@pm.me>
2023-10-19 02:17:19 -0400

    StyleCop Fixes
    Addressed issues with style and xml documentation

--- bobbylite.realmikefacts.web/Pages/Index.cshtml.cs
+++ bobbylite.realmikefacts.web/Pages/Index.cshtml.cs
@@ -1,19 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace bobbylite.realmikefacts.web.Pages;
 
+/// <summary>
+/// Index.
+/// </summary>
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IndexModel"/> class.
+    /// </summary>
+    /// <param name="logger">Logger from DI.</param>
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// Runs on get.
+    /// </summary>
     public void OnGet()
     {
-
     }
 }
