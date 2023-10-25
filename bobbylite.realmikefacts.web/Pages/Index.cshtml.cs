14b6432d7cd666330aa0170877bdd1394955379d
Bobby <bobbielite@pm.me>
2023-10-25 03:40:40 -0400

    Bugfix
    Was missing delete cookie condition causing false Access denied edge case

--- bobbylite.realmikefacts.web/Pages/Index.cshtml.cs
+++ bobbylite.realmikefacts.web/Pages/Index.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Text;
+using System.Text.Json;
+using bobbylite.realmikefacts.web.Models.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace bobbylite.realmikefacts.web.Pages;
 
@@ -23,10 +26,33 @@ public class IndexModel : PageModel
     /// </summary>
     public void OnGet()
     {
-        var cookie = HttpContext.Request.Cookies[".AspNetCore.Custom.Auth.Cookies"];
-        bool isAuthenticated = User.Identity?.IsAuthenticated ?? false;
-        if ((!isAuthenticated) && (cookie is not null))
+        const string cookieKey = ".AspNetCore.Custom.Auth.Cookies";
+        const string nameIdentifierKey = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+        
+        var cookie = HttpContext.Request.Cookies[cookieKey];
+        if (cookie is null)
         {
+            return;
+        }
+
+        bool isMyCookie = false;
+        
+        byte[] bytes = Convert.FromBase64String(cookie);
+        var serializedJson = Encoding.ASCII.GetString(bytes);
+        var groupAuthorizationModel = JsonSerializer.Deserialize<GroupAuthorizationModel>(serializedJson);
+        
+        if (groupAuthorizationModel is null)
+        {
+            throw new NullObjectException();
+        }
+        
+        var userId = HttpContext.User.FindFirst(c => c.Type == nameIdentifierKey)!.Value;
+        isMyCookie = userId == groupAuthorizationModel.UserId;
+        bool isAuthenticated = User.Identity?.IsAuthenticated ?? false;
+        
+        if (!isAuthenticated || !isMyCookie)
+        {
+            
             HttpContext.Response.Cookies.Delete(".AspNetCore.Custom.Auth.Cookies");
         }
     }
