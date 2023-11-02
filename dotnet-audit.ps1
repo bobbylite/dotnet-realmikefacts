a0244434fc000aa0b20ae121a827651cb4d40c15
Bobby <bobbielite@pm.me>
2023-11-02 00:46:19 -0400

    Refactor

--- dotnet-audit.ps1
+++ dotnet-audit.ps1
@@ -1,20 +1,8 @@
-#!/usr/bin/env bash
-
-# There is currently no support for machine readable output from `dotnet list package --vulnerable`.
-# See: https://github.com/NuGet/Home/issues/7752
-# See: https://github.com/dotnet/sdk/issues/16852
-#
-# We were using Retire.NET but that has been deprecated as of 5.0.0 and does not run on .NET 6.0.
-# See: https://github.com/RetireNet/dotnet-retire/issues/75
-
-# Must be run first for clean environments.
 dotnet restore
 
-# Get the output of the package analysis.
 $output = dotnet list package --vulnerable
 Write-Output $output
 
-# Check if the output contains "following vulnerable".
 if ($output -match "Project ``bobbylite.realmikefacts.web`` has the following vulnerable packages") {
     exit 1
 }
